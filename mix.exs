77d7ac9913b0d336e3543cbe0c0c7aa18a0ace81
NeuroWinter <alex@pikori.com>
2024-09-25 09:38:23 +1200

    build(mix.exs): added ex_doc as a dev dependency

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule ElixirCrt.MixProject do
     [
       {:httpoison, "~> 2.0"},
       {:poison, "~> 5.0"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
